7587d27a665251972ab12487b6691b2663be7db3
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-02-26 14:29:19 +0200

    System administrators may update or delete

--- organizations/collection/permissions.js
+++ organizations/collection/permissions.js
@@ -1,2 +1,17 @@
 // Logged in user may create organization
 Organzations.permit('insert').apply();
+
+// Administrator role can delete
+Organizations.permit('delete').ifHasRole("admin").apply();
+
+Security.defineMethod("ifUserIsOrganizationAdmin", {
+  fetch: [],
+  transform: null,
+  deny: function (type, arg, userId, organization) {
+    // Get a list of organization admins
+    const adminIds = organization.adminIds;
+
+    // Check if user is organization admin
+    return _.contains(adminIds, userId);
+  }
+});
