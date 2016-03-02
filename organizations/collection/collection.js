a9fadcb8b0dec24ec4d550b0bf2458924355aa88
Damir Mustafin <frenchthebread@gmail.com>
2016-03-02 16:02:10 +0200

    Added userIsAdmin collection helper

--- organizations/collection/collection.js
+++ organizations/collection/collection.js
@@ -1,1 +1,12 @@
 Organizations = new Mongo.Collection("organizations");
+
+Organizations.helpers({
+    userIsAdmin: function (userId) {
+
+        // Assign organization instance to a variable
+        var organization = this;
+
+        // Check if administratorIds contains userId that was passed and returns boolean
+        return _.contains(organization.administratorIds, userId);
+    }
+});
