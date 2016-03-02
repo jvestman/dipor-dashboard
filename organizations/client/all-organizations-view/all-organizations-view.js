eedbbcc03ccf1d004a3d9eb8ea7f09a9b2ef9aa7
Damir Mustafin <frenchthebread@gmail.com>
2016-03-02 11:20:41 +0200

    Comments

--- organizations/client/all-organizations-view/all-organizations-view.js
+++ organizations/client/all-organizations-view/all-organizations-view.js
@@ -1,7 +1,9 @@
 Template.allOrganizationsView.created = function () {
 
+    // Get reference to template instance
     var instance = this;
 
+    // Subscribe to allOrganizations publication
     instance.subscribe('allOrganizations');
 
 };
