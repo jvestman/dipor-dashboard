9ef20d9a9b78194474af3fa26344a17eb0eeac4c
Damir Mustafin <frenchthebread@gmail.com>
2016-03-02 16:33:44 +0200

    Added server-side check "currentUserIsOrganizationAdmin"

--- organizations/server/methods.js
+++ organizations/server/methods.js
@@ -1,13 +1,29 @@
 Meteor.methods({
     updateOrganization: function (organizationId, organizationData) {
 
-        Organizations.update(organizationId, {
-            $set: {
-                name: organizationData.name,
-                description: organizationData.description,
-                updatedAt: new Date()
-            }
-        });
+        // Get current user's Id
+        var currentUserId = ''; // TODO: change '' to Meteor.userId() when authentication is implemented
+
+        // Check if current user is organization admin using collection helper
+        var currentUserIsOrganizationAdmin = Organizations.findOne(organizationId).userIsAdmin(currentUserId);
+
+        // Check if specific organization document is found
+        if (currentUserIsOrganizationAdmin) {
+
+            // Update organization data if Ok
+            Organizations.update(organizationId, {
+                $set: {
+                    name: organizationData.name,
+                    description: organizationData.description,
+                    updatedAt: new Date()
+                }
+            });
+
+        } else {
+
+            // If check failed throw an error
+            throw new Meteor.Error("Permission denied. Only Administrators are able to modify organizations.");
+        }
 
     }
 });
