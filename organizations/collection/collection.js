96b36b99570bef532ce23dff3c5cfe542e0b2e8b
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-03-03 13:38:33 +0200

    Allow all for now.

--- organizations/collection/collection.js
+++ organizations/collection/collection.js
@@ -1,1 +1,14 @@
 Organizations = new Mongo.Collection("organizations");
+
+// Access control for organization collection
+Organizations.allow({
+  insert: function() {
+    return true;
+  },
+  update: function() {
+    return true;
+  },
+  remove: function() {
+    return true;
+  }
+});
