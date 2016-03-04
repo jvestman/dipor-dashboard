c183c78a5bc09c36f1d79fc639b54fb96708e2c1
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-03-04 11:27:43 +0200

    Move permissions

--- organizations/collection/collection.js
+++ organizations/collection/collection.js
@@ -1,14 +1,1 @@
 Organizations = new Mongo.Collection("organizations");
-
-// Access control for organization collection
-Organizations.allow({
-  insert: function() {
-    return true;
-  },
-  update: function() {
-    return true;
-  },
-  remove: function() {
-    return true;
-  }
-});
---
+++ organizations/collection/permissions.js
@@ -0,0 +1,12 @@
+// Access control for organization collection
+Organizations.allow({
+  insert: function() {
+    return true;
+  },
+  update: function() {
+    return true;
+  },
+  remove: function() {
+    return true;
+  }
+});
