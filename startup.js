c46a49fe301eefe278bf90ccdced8fac386e4e00
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-03-04 11:27:59 +0200

    Move startup

--- startup.js
+++
@@ -1,8 +0,0 @@
-if(Meteor.isServer) {
-  Meteor.startup(function() {
-    // Define collections exposed for REST API
-    SimpleRest.configure({
-      collections: ["organizations"]
-    });
-  });
-}
