066c2c96875e69a1c340bc305de4969c39a57ad5
illya nizyev <nzv@protonmail.ch>
2016-03-06 10:29:50 +0200

    clean up

--- organizations/client/single-organization-view/single-organization-view.js
+++ organizations/client/single-organization-view/single-organization-view.js
@@ -1,106 +1,105 @@
 Template.singleOrganizationView.created = function () {
+  // Get reference to template instance
+  const instance = this;
 
-    // Get reference to template instance
-    const instance = this;
+  // Get current organization Id
+  instance.organizationId = FlowRouter.current().params.organizationId;
 
-    // Get current organization Id
-    instance.organizationId = FlowRouter.current().params.organizationId;
+  // Subscribe to singleOrganization publication and pass organization Id
+  instance.subscribe('singleOrganization', instance.organizationId);
 
-    // Subscribe to singleOrganization publication and pass organization Id
-    instance.subscribe('singleOrganization', instance.organizationId);
+  // Initialise reactive variable
+  instance.editOrganizationMode = new ReactiveVar(false);
 
-    // Initialise reactive variable
-    instance.editOrganizationMode = new ReactiveVar(false);
-
-    instance.autorun(function () {
-      if (instance.subscriptionsReady()) {
-        instance.organization = Organizations.findOne(instance.organizationId);
-      }
-    });
+  instance.autorun(function () {
+    if (instance.subscriptionsReady()) {
+      instance.organization = Organizations.findOne(instance.organizationId);
+    }
+  });
 };
 
 Template.singleOrganizationView.helpers({
-    singleOrganization: function () {
-        // Get reference to template instance
-        const instance = Template.instance();
+  singleOrganization: function () {
+    // Get reference to template instance
+    const instance = Template.instance();
 
-        // Fetch organization data and pass current organization Id
-        return instance.organization;
-    },
-    editOrganizationMode: function () {
-        // Get reference to template instance
-        const instance = Template.instance();
+    // Fetch organization data and pass current organization Id
+    return instance.organization;
+  },
+  editOrganizationMode: function () {
+    // Get reference to template instance
+    const instance = Template.instance();
 
-        // Get reactive var value
-        return instance.editOrganizationMode.get();
-    },
-    currentUserIsOrganizationAdmin: function () {
-        // Get reference to template instance
-        const instance = Template.instance();
+    // Get reactive var value
+    return instance.editOrganizationMode.get();
+  },
+  currentUserIsOrganizationAdmin: function () {
+    // Get reference to template instance
+    const instance = Template.instance();
 
-        // Check if current user is organization admin using collection helper
-        return instance.organization.currentUserIsAdmin();
-    }
+    // Check if current user is organization admin using collection helper
+    return instance.organization.currentUserIsAdmin();
+  }
 });
 
 Template.singleOrganizationView.events({
-    'click #editOrganizationMode': function (event) {
-        event.preventDefault();
+  'click #editOrganizationMode': function (event) {
+    event.preventDefault();
 
-        // Get reference to template instance
-        const instance = Template.instance();
+    // Get reference to template instance
+    const instance = Template.instance();
 
-        // Initialize medium editor
-        instance.organizationEditor = new MediumEditor('.editable', {
-            toolbar: false,
-            disableReturn: true,
-            disableExtraSpaces: true
-        });
+    // Initialize medium editor
+    instance.organizationEditor = new MediumEditor('.editable', {
+      toolbar: false,
+      disableReturn: true,
+      disableExtraSpaces: true
+    });
 
-        // Update reactive variable
-        instance.editOrganizationMode.set(true);
-    },
-    'click #cancelEditOrganizationMode': function (event) {
-        event.preventDefault();
+    // Update reactive variable
+    instance.editOrganizationMode.set(true);
+  },
+  'click #cancelEditOrganizationMode': function (event) {
+    event.preventDefault();
 
-        // Get reference to template instance
-        const instance = Template.instance();
+    // Get reference to template instance
+    const instance = Template.instance();
 
-        // Get existing organization text
-        const organization = instance.organization;
+    // Get existing organization text
+    const organization = instance.organization;
 
-        // Deconstruct medium-editor
-        instance.organizationEditor.destroy();
+    // Deconstruct medium-editor
+    instance.organizationEditor.destroy();
 
-        // Update reactive variable
-        instance.editOrganizationMode.set(false);
+    // Update reactive variable
+    instance.editOrganizationMode.set(false);
 
-        // Reset UI text to current value
-        $('#organizationName').text(organization.name);
-        $('#organizationDescription').text(organization.description);
-    },
-    'click #updateOrganization': function (event) {
-        event.preventDefault();
+    // Reset UI text to current value
+    $('#organizationName').text(organization.name);
+    $('#organizationDescription').text(organization.description);
+  },
+  'click #updateOrganization': function (event) {
+    event.preventDefault();
 
-        // Get reference to template instance
-        const instance = Template.instance();
+    // Get reference to template instance
+    const instance = Template.instance();
 
-        // Get organization reference
-        const organizationId = instance.organizationId
+    // Get organization reference
+    const organizationId = instance.organizationId
 
-        // Update organization data
-        Organizations.update(organizationId, {
-            $set: {
-                name: $('#organizationName').text(),
-                description: $('#organizationDescription').text(),
-                updatedAt: new Date()
-            }
-        });
+    // Update organization data
+    Organizations.update(organizationId, {
+      $set: {
+        name: $('#organizationName').text(),
+        description: $('#organizationDescription').text(),
+        updatedAt: new Date()
+      }
+    });
 
-        // Deconstruct medium-editor
-        instance.organizationEditor.destroy();
+    // Deconstruct medium-editor
+    instance.organizationEditor.destroy();
 
-        // Update reactive variable
-        instance.editOrganizationMode.set(false);
-    }
+    // Update reactive variable
+    instance.editOrganizationMode.set(false);
+  }
 });
