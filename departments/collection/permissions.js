87e483998f2e97e34b82b6c478ffc0b4c809b741
Damir Mustafin <frenchthebread@gmail.com>
2016-03-07 21:05:47 +0200

    Permission fix

--- departments/collection/permissions.js
+++ departments/collection/permissions.js
@@ -1,10 +1,18 @@
 Departments.allow({
   insert: function (userId, department) {
-    // Only allow department administrators to insert
-    return department.currentUserIsAdmin();
+
+    // Get organization Id
+    const organizationId = department.organizationId;
+
+    // Only allow organization administrators to insert
+    return Organizations.findOne(organizationId).currentUserIsAdmin();
   },
   update: function (userId, department) {
-    // Only allow department administrators to edit
-    return department.currentUserIsAdmin();
+
+    // Get organization Id
+    const organizationId = department.organizationId;
+
+    // Only allow organization administrators to insert
+    return Organizations.findOne(organizationId).currentUserIsAdmin();
   }
 });
