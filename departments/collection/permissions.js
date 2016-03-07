12804317c03055f09cbc6484e590ab050fc22212
Damir Mustafin <frenchthebread@gmail.com>
2016-03-07 18:53:42 +0200

    Update permissions (insert/update)

--- departments/collection/permissions.js
+++ departments/collection/permissions.js
@@ -1,5 +1,10 @@
 Departments.allow({
-  insert: function () {
-    return true;
+  insert: function (userId, department) {
+    // Only allow department administrators to insert
+    return department.currentUserIsAdmin();
+  },
+  update: function (userId, department) {
+    // Only allow department administrators to edit
+    return department.currentUserIsAdmin();
   }
 });
