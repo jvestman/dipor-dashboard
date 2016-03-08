c1535a42acb3ea76732a9eab029fd2e3fb7d8512
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-08 13:42:15 +0200

    Call addOrganizationMember method with email

--- organizations/client/single/members/add/add.js
+++ organizations/client/single/members/add/add.js
@@ -3,7 +3,16 @@ Template.addOrganizationMember.events({
     // Prevent the form from submitting
     event.preventDefault();
 
+    // Get email from form
     const email = $("#email").val();
+
+    // Add member to organization, checking if email is registered
+    Meteor.call("addOrganizationMember", email, function (error, success) {
+      if (error) {
+        // Display any error to the user
+        alert(error);
+      }
+    });
 
     // Hide the add organization member modal
     Modal.hide("addOrganizationMember");
