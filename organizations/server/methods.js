fc2489210793ef207a8a384ddf20af33addf02a6
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-08 14:22:04 +0200

    Add authorization check to addOrganizationMember

--- organizations/server/methods.js
+++ organizations/server/methods.js
@@ -9,8 +9,17 @@ Meteor.methods({
     // If user account does not exist, throw error
     if (!userAccount) {
       throw new Meteor.Error( 500, 'Could not find user' );
-    }
+    } else {
+      // Get user ID
+      const userId = userAccount._id;
 
-    //console.log(userAccount);
+      // Get organization by ID
+      const organization = Organizations.findOne(organizationId);
+
+      // Make sure current user is organization administrator
+      if (organization.currentUserIsAdmin()){
+        // Add user ID to organization member IDs array
+      }
+    }
   }
 });
