9a5ecc5ca1abf9ef12bf5a9db469eda7ae5460f4
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-08 14:13:50 +0200

    Add details object with email and organizationId

--- organizations/client/single/members/add/add.js
+++ organizations/client/single/members/add/add.js
@@ -3,11 +3,22 @@ Template.addOrganizationMember.events({
     // Prevent the form from submitting
     event.preventDefault();
 
-    // Get email from form
-    const email = $("#email").val();
+    // Create object to hold details for current submission
+    const details = {};
 
+    // Get reference to template instance
+    const instance = Template.instance();
+
+    const organizationId = instance.data.organizationId;
+
+    // Get email from form
+    details.email = $("#email").val();
+
+    // Add organization ID
+    details.organizationId = organizationId;
+    
     // Add member to organization, checking if email is registered
-    Meteor.call("addOrganizationMember", email, function (error, success) {
+    Meteor.call("addOrganizationMember", details, function (error, success) {
       if (error) {
         // Display any error to the user
         alert(error);
