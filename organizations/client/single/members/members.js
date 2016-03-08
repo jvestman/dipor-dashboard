57f65b6f9445e4ea501d55506bdc88a8a4e0dd5c
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-08 14:12:09 +0200

    Pass organizationId to addOrganizationMember

--- organizations/client/single/members/members.js
+++ organizations/client/single/members/members.js
@@ -1,6 +1,11 @@
 Template.organizationMembers.events({
   "click #add-member": function () {
+    // Get reference to template instance
+    const instance = Template.instance();
+
+    const organizationId = instance.data.organizationId;
+
     // Show the add member modal
-    Modal.show("addOrganizationMember");
+    Modal.show("addOrganizationMember", {organizationId});
   }
 });
