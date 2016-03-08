b048f0686910b524a61b5ea06c6a245dbff7e118
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-08 14:18:16 +0200

    Add comment

--- organizations/server/methods.js
+++ organizations/server/methods.js
@@ -6,7 +6,7 @@ Meteor.methods({
     // Find user given email
     const userAccount = Accounts.findUserByEmail(email);
 
-
+    // If user account does not exist, throw error
     if (!userAccount) {
       throw new Meteor.Error( 500, 'Could not find user' );
     }
