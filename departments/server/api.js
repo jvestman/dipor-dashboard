d7a79def41189c34c8e78030710f0f68177686c3
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-03-09 16:48:44 +0200

    Require authentication for all

--- departments/server/api.js
+++ departments/server/api.js
@@ -1,3 +1,7 @@
 // Generates: GET, POST on /api/departments and GET, PUT, DELETE on
 // /api/departments/:id for the Departments collection
-Api.addCollection(Departments);
+ApiV1.addCollection(Departments, {
+  routeOptions: {
+    authRequired: true
+  }
+});
--- organizations/server/api.js
+++ organizations/server/api.js
@@ -1,3 +1,7 @@
 // Generates: GET, POST on /api/organizations and GET, PUT, DELETE on
 // /api/organizations/:id for the Organizations collection
-Api.addCollection(Organizations);
+ApiV1.addCollection(Organizations, {
+  routeOptions: {
+    authRequired: true
+  }
+});
