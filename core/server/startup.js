bf7e7ca2646fcd77aeb80d9013cdb9d3b13ae518
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-03-09 14:30:33 +0200

    Rename "main" to "core" to get it loaded first

---
+++ core/server/startup.js
@@ -0,0 +1,9 @@
+// Global API configuration
+Api = new Restivus({
+  useDefaultAuth: false,
+  prettyJson: true
+});
+
+Meteor.startup(function() {
+  // Initial things
+});
--- main/server/startup.js
+++
@@ -1,7 +0,0 @@
-Meteor.startup(function() {
-  // Global API configuration
-  let Api = new Restivus({
-    useDefaultAuth: false,
-    prettyJson: true
-  });
-});
