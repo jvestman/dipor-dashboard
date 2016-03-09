6524bb8c0e7413fc8cd3855cc7bfb974cd811de8
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-09 13:36:17 +0200

    Remove Meteor.startup() wrapper

--- accounts/accounts.js
+++ accounts/accounts.js
@@ -1,59 +1,57 @@
-Meteor.startup(function () {
-  AccountsTemplates.configure({
-    // Router configuration
-    defaultLayout: "mainLayout",
-    defaultContentRegion: "main",
+AccountsTemplates.configure({
+  // Router configuration
+  defaultLayout: "mainLayout",
+  defaultContentRegion: "main",
 
-    // Behavior
-    confirmPassword: true,
-    enablePasswordChange: true,
-    forbidClientAccountCreation: false,
-    overrideLoginErrors: false,
-    sendVerificationEmail: false,
-    lowercaseUsername: true,
-    focusFirstInput: true,
+  // Behavior
+  confirmPassword: true,
+  enablePasswordChange: true,
+  forbidClientAccountCreation: false,
+  overrideLoginErrors: false,
+  sendVerificationEmail: false,
+  lowercaseUsername: true,
+  focusFirstInput: true,
 
-    // Appearance
-    showAddRemoveServices: false,
-    showForgotPasswordLink: false,
-    showLabels: true,
-    showPlaceholders: true,
-    showResendVerificationEmailLink: false,
+  // Appearance
+  showAddRemoveServices: false,
+  showForgotPasswordLink: false,
+  showLabels: true,
+  showPlaceholders: true,
+  showResendVerificationEmailLink: false,
 
-    // Client-side Validation
-    continuousValidation: true,
-    negativeFeedback: false,
-    negativeValidation: true,
-    positiveValidation: true,
-    positiveFeedback: true,
-    showValidating: true,
+  // Client-side Validation
+  continuousValidation: true,
+  negativeFeedback: false,
+  negativeValidation: true,
+  positiveValidation: true,
+  positiveFeedback: true,
+  showValidating: true,
 
-    // Privacy Policy and Terms of Use
-    //privacyUrl: 'privacy',
-    //termsUrl: 'terms-of-use',
+  // Privacy Policy and Terms of Use
+  //privacyUrl: 'privacy',
+  //termsUrl: 'terms-of-use',
 
-    // Redirects
-    homeRoutePath: '/',
-    redirectTimeout: 4000,
+  // Redirects
+  homeRoutePath: '/',
+  redirectTimeout: 4000,
 
-    // Hooks
-    //onLogoutHook: myLogoutFunc,
-    //onSubmitHook: mySubmitFunc,
-    //preSignUpHook: myPreSubmitFunc,
-    //postSignUpHook: myPostSubmitFunc,
+  // Hooks
+  //onLogoutHook: myLogoutFunc,
+  //onSubmitHook: mySubmitFunc,
+  //preSignUpHook: myPreSubmitFunc,
+  //postSignUpHook: myPostSubmitFunc,
 
-    // Texts
-    // texts: {
-    //   button: {
-    //       signUp: "Register"
-    //   },
-    //   socialSignUp: "Register",
-    //   socialIcons: {
-    //       "meteor-developer": "fa fa-rocket"
-    //   },
-    //   title: {
-    //       forgotPwd: "Recover Your Password"
-    //   },
-    // },
-  });
+  // Texts
+  // texts: {
+  //   button: {
+  //       signUp: "Register"
+  //   },
+  //   socialSignUp: "Register",
+  //   socialIcons: {
+  //       "meteor-developer": "fa fa-rocket"
+  //   },
+  //   title: {
+  //       forgotPwd: "Recover Your Password"
+  //   },
+  // },
 });
