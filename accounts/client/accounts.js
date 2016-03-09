8b07fd12b43f5b116c0d03b976e1b4c0226ec02d
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-09 13:16:46 +0200

    Add basic accounts configuration

---
+++ accounts/client/accounts.js
@@ -0,0 +1,59 @@
+Meteor.startup(function () {
+  AccountsTemplates.configure({
+    // Router configuration
+    defaultLayout: "mainLayout",
+    defaultContentRegion: "main",
+
+    // Behavior
+    confirmPassword: true,
+    enablePasswordChange: true,
+    forbidClientAccountCreation: false,
+    overrideLoginErrors: false,
+    sendVerificationEmail: false,
+    lowercaseUsername: true,
+    focusFirstInput: true,
+
+    // Appearance
+    showAddRemoveServices: false,
+    showForgotPasswordLink: false,
+    showLabels: true,
+    showPlaceholders: true,
+    showResendVerificationEmailLink: false,
+
+    // Client-side Validation
+    continuousValidation: true,
+    negativeFeedback: false,
+    negativeValidation: true,
+    positiveValidation: true,
+    positiveFeedback: true,
+    showValidating: true,
+
+    // Privacy Policy and Terms of Use
+    //privacyUrl: 'privacy',
+    //termsUrl: 'terms-of-use',
+
+    // Redirects
+    homeRoutePath: '/',
+    redirectTimeout: 4000,
+
+    // Hooks
+    //onLogoutHook: myLogoutFunc,
+    //onSubmitHook: mySubmitFunc,
+    //preSignUpHook: myPreSubmitFunc,
+    //postSignUpHook: myPostSubmitFunc,
+
+    // Texts
+    // texts: {
+    //   button: {
+    //       signUp: "Register"
+    //   },
+    //   socialSignUp: "Register",
+    //   socialIcons: {
+    //       "meteor-developer": "fa fa-rocket"
+    //   },
+    //   title: {
+    //       forgotPwd: "Recover Your Password"
+    //   },
+    // },
+  });
+});
