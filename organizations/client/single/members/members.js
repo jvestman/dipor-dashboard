4519f57c9eef98e51400ce33188662e1afa57475
Damir Mustafin <frenchthebread@gmail.com>
2016-03-11 12:43:34 +0200

    Added template helper & cleanup

--- organizations/client/single/members/members.js
+++ organizations/client/single/members/members.js
@@ -2,24 +2,35 @@ Template.organizationMembers.onCreated(function(){
   // Get reference to template instance
   const instance = this;
 
-  instance.organizationId = instance.data.organization._id;
+  // Get organization Id that was passed to a template
+  instance.organizationId = instance.data.organizationId;
 
   // Subscribe to organization members
   instance.subscribe("organizationMembers", instance.organizationId);
-
-  instance.autorun(function () {
-    console.log(instance.data.organization);
-  });
+  
 });
 
 Template.organizationMembers.events({
   "click #add-member": function () {
+
     // Get reference to template instance
     const instance = Template.instance();
 
+    // Get organization Id
     const organizationId = instance.organizationId;
 
     // Show the add member modal
     Modal.show("addOrganizationMember", {organizationId});
+  }
+});
+
+Template.organizationMembers.helpers({
+  "organization": function () {
+
+    // Get reference to a template instance
+    const instance = Template.instance();
+
+    // Return organization with provided otganization Id
+    return Organizations.findOne(instance.organizationId);
   }
 });
