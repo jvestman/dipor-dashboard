583449c8ee3ed9b495a9a5acb60857e86bc629b8
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-03-14 12:19:02 +0200

    Add swagger config

--- core/server/api.js
+++ core/server/api.js
@@ -10,7 +10,22 @@ API.v1 = new Restivus({
   },
   useDefaultAuth: true,
   prettyJson: true,
-  enableCors: true
+  enableCors: true,
+  swagger: {
+    swagger: "2.0",
+    info: {
+      version: "1.0.0",
+      title: "Dipor API",
+      description: "Dipor REST API",
+      termsOfService: "https://dashboard.digipalvelutehdas.fi/terms/",
+      contact: {
+        name: "Dipor team"
+      },
+      license: {
+        name: "MIT"
+      }
+    }
+  }
 });
 
 // Generates: POST on /api/v1/users and GET, DELETE /api/v1/users/:id for
