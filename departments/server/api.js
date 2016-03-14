862816ef810e59da6c01a71153219abf155c70ad
Ville Jyrkkä <ville.jyrkka@samposoftware.com>
2016-03-14 17:46:12 +0200

    Add swagger meta for departments and organizations

--- departments/server/api.js
+++ departments/server/api.js
@@ -3,17 +3,79 @@
 API.v1.addCollection(Departments, {
   routeOptions: {
     authRequired: true
+  },
+  endpoints: {
+    get: {
+      swagger: {
+        description: "Returns one department with given ID.",
+        responses: {
+          "200": {
+            description: "One department"
+          }
+        }
+      }
+    },
+    post: {
+      swagger: {
+        description: "Add department.",
+        responses: {
+          "200": {
+            description: "Return department that was added."
+          }
+        }
+      }
+    },
+    put: {
+      swagger: {
+        description: "Update department with given ID.",
+        responses: {
+          "200": {
+            description: "Returns updated department."
+          }
+        }
+      }
+    },
+    delete: {
+      swagger: {
+        description: "Delete department with given ID.",
+        responses: {
+          "200": {
+            description: "Successful delete."
+          }
+        }
+      }
+    },
+    getAll: {
+      swagger: {
+        description: "Returns all departments.",
+        responses: {
+          "200": {
+            description: "List of departments."
+          }
+        }
+      }
+    }
   }
 });
 
 // Maps to: /api/v1/organizations/:id/departments
 API.v1.addRoute('organizations/:id/departments', {authRequired: true}, {
-  get: function () {
-    // Get organizationId from URL parameters
-    const organizationId = this.urlParams.id;
-    // Fetch all departments of the given organization
-    const departments = Departments.find({ "organizationId": organizationId }).fetch();
+  get: {
+    swagger: {
+      description: "Returns all departments of given organization ID.",
+      responses: {
+        "200": {
+          description: "List of departments."
+        }
+      }
+    },
+    action: function () {
+      // Get organizationId from URL parameters
+      const organizationId = this.urlParams.id;
+      // Fetch all departments of the given organization
+      const departments = Departments.find({ "organizationId": organizationId }).fetch();
 
-    return departments;
+      return departments;
+    }
   }
 });
--- organizations/server/api.js
+++ organizations/server/api.js
@@ -3,5 +3,57 @@
 API.v1.addCollection(Organizations, {
   routeOptions: {
     authRequired: true
+  },
+  endpoints: {
+    get: {
+      swagger: {
+        description: "Returns one organization with given ID.",
+        responses: {
+          "200": {
+            description: "One organization"
+          }
+        }
+      }
+    },
+    post: {
+      swagger: {
+        description: "Add organization.",
+        responses: {
+          "200": {
+            description: "Return organization that was added."
+          }
+        }
+      }
+    },
+    put: {
+      swagger: {
+        description: "Update organization with given ID.",
+        responses: {
+          "200": {
+            description: "Returns updated organization."
+          }
+        }
+      }
+    },
+    delete: {
+      swagger: {
+        description: "Delete organization with given ID.",
+        responses: {
+          "200": {
+            description: "Successful delete."
+          }
+        }
+      }
+    },
+    getAll: {
+      swagger: {
+        description: "Returns all organizations.",
+        responses: {
+          "200": {
+            description: "List of organizations."
+          }
+        }
+      }
+    }
   }
 });
