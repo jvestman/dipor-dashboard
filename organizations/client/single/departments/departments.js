6fa6c322081a9a4909606a50f7494fafbe7242d8
Damir Mustafin <frenchthebread@gmail.com>
2016-03-14 18:04:53 +0200

    Pass organization object instead of Id

--- organizations/client/single/departments/departments.js
+++ organizations/client/single/departments/departments.js
@@ -3,20 +3,18 @@ Template.organizationDepartments.created = function () {
   const instance = this;
 
   // Get current organization Id
-  instance.organizationId = instance.data.organizationId;
+  instance.organization = instance.data.organization;
 
   // Subscribe to allOrganizations publication
-  instance.subscribe('organizationDepartments', instance.organizationId);
-  instance.subscribe('singleOrganization', instance.organizationId);
+  instance.subscribe('organizationDepartments', instance.organization._id);
+  instance.subscribe('singleOrganization', instance.organization._id);
 
   instance.autorun(function () {
     if (instance.subscriptionsReady()) {
 
       // Get all departments for particular organization
-      instance.departments = Departments.find({organizationId: instance.organizationId});
+      instance.departments = Departments.find({organizationId: instance.organization._id});
 
-      // Get organization
-      instance.organization = Organizations.findOne(instance.organizationId);
     }
   });
 };
