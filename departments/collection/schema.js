4752c253d588b457d85aa92b369e9a8f1a648feb
Damir Mustafin <frenchthebread@gmail.com>
2016-03-16 15:48:40 +0200

    Departments schema change, added auto values

--- departments/collection/schema.js
+++ departments/collection/schema.js
@@ -24,6 +24,29 @@ Departments.schema = new SimpleSchema({
   "administratorIds.$": {
     "type": String,
     "regEx": SimpleSchema.RegEx.Id
+  },
+  createdAt: {
+    type: Date,
+    autoValue: function () {
+
+      // Check if mongoDB insert operation is initial
+      if (this.isInsert)
+        return new Date();
+
+      // Check if mongoDB insert operation is initial
+      else if (this.isUpsert)
+        return { $setOnInsert: new Date() };
+
+      // If not - field is not updated
+      else
+        this.unset();
+    }
+  },
+  updatedAt: {
+    type: Date,
+    autoValue: function () {
+      return new Date();
+    }
   }
 });
 
