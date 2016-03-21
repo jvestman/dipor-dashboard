d9f3fca56aad252e7e8333bd22acc3749aadf91b
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-21 15:19:27 +0200

    Add singleService publication

--- services/collection/server/publications.js
+++ services/collection/server/publications.js
@@ -1,6 +1,15 @@
+/* All services */
 Meteor.publish("allServices", funciton () {
   // Find all Services (database pointer)
   const allServices = Services.find();
 
   return allServices;
+});
+
+/* Single service */
+Meteor.publish("singleService", funciton (serviceId) {
+  // Find single service by ID (database pointer)
+  const singleService = Services.find(serviceId);
+
+  return singleService;
 });
