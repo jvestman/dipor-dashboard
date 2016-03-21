6c16ee76ccea83604688b6fccd0be1bd844eaf3e
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-21 15:25:48 +0200

    Fix typos

--- services/collection/server/publications.js
+++ services/collection/server/publications.js
@@ -1,5 +1,5 @@
 /* All services */
-Meteor.publish("allServices", funciton () {
+Meteor.publish("allServices", function () {
   // Find all Services (database pointer)
   const allServices = Services.find();
 
@@ -7,7 +7,7 @@ Meteor.publish("allServices", funciton () {
 });
 
 /* Single service */
-Meteor.publish("singleService", funciton (serviceId) {
+Meteor.publish("singleService", function (serviceId) {
   // Find single service by ID (database pointer)
   const singleService = Services.find(serviceId);
 
@@ -15,7 +15,7 @@ Meteor.publish("singleService", funciton (serviceId) {
 });
 
 /* Service(s) for a given department */
-Meteor.publish("departmentServices", funciton (departmentId) {
+Meteor.publish("departmentServices", function (departmentId) {
   // Find all services for a given department (database pointer)
   const departmentServices = Services.find({ departmentId: departmentId });
 
