3835bf0598021b6390dedb42cea7000e52c7ab75
Damir Mustafin <frenchthebread@gmail.com>
2016-03-30 01:41:13 +0300

    Remove breadcrumb

--- services/router.js
+++ services/router.js
@@ -1,6 +1,6 @@
 FlowRouter.route('/service/:serviceId', {
   name: 'singleService',
   action: function (params) {
-    BlazeLayout.render("mainLayout", { main: "singleService", breadcrumb: "singleServiceBreadcrumb" });
+    BlazeLayout.render("mainLayout", { main: "singleService" });
   }
 });
