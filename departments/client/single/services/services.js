2d49869392406555d4846d0fc1c187aa11028992
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-31 17:07:40 +0300

    Add department helper

--- departments/client/single/services/services.js
+++ departments/client/single/services/services.js
@@ -7,3 +7,15 @@ Template.departmentServices.onCreated(function () {
 
   instance.subscribe("departmentServices", instance.departmentId)
 });
+
+Template.departmentServices.helpers({
+  department () {
+    // Get reference to template instance
+    const instance = this;
+
+    // Get current department using ID
+    const department = Departments.findOne(instance.departmentId);
+
+    return department;
+  }
+});
