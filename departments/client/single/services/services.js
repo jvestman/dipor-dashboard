0d3f966eb857083e20e27e3fd23bd11857c02f87
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-31 15:47:22 +0300

    Get department ID in onCreated callback

--- departments/client/single/services/services.js
+++ departments/client/single/services/services.js
@@ -1,3 +1,7 @@
 Template.departmentServices.onCreated(function () {
-  
+  // Get reference to template instance
+  const instance = this;
+
+  // Get department ID from template data context
+  instance.departmentId = instance.data.departmentId;
 });
