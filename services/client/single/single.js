2df1936fdab9d30fb40191861491627a45ddd856
Damir Mustafin <frenchthebread@gmail.com>
2016-04-01 05:51:40 +0300

    init MediumEditor & set up event listeners

--- services/client/single/single.js
+++ services/client/single/single.js
@@ -8,6 +8,14 @@ Template.singleService.created = function () {
 
   // Subscribe to singleService publication
   instance.subscribe("singleService", instance.serviceId);
+
+  instance.editMode = new ReactiveVar(false);
+
+  instance.autorun(() => {
+    if (instance.subscriptionsReady()) {
+      instance.service = Services.findOne(instance.serviceId);
+    }
+  });
 };
 
 Template.singleService.helpers({
@@ -17,6 +25,80 @@ Template.singleService.helpers({
     const instance = Template.instance();
 
     // Fetch service by Id and return
-    return Services.findOne(instance.serviceId);
+    return instance.service;
+  },
+  editMode () {
+
+    const instance = Template.instance();
+
+    return instance.editMode.get();
   }
 });
+
+Template.singleService.events({
+  'click #edit-service': function (event) {
+
+    event.preventDefault();
+
+    // Get reference to template instance
+    const instance = Template.instance();
+
+    instance.serviceEditor = new MediumEditor('.editable', {
+      toolbar: false,
+      disableReturn: true,
+      disableExtraSpaces: true
+    });
+
+    // Update reactive variable
+    instance.editMode.set(true);
+  },
+  'click #cancel-edit': function (event) {
+
+    event.preventDefault();
+
+    // Get reference to template instance
+    const instance = Template.instance();
+
+    // Get existing service object
+    const service = instance.service;
+
+    // Deconstruct medium-editor
+    instance.serviceEditor.destroy();
+
+    // Update reactive variable
+    instance.editMode.set(false);
+
+    // Reset UI text to current value
+    $('#service-name').text(service.name);
+    $('#service-description').text(service.description);
+    $('#service-version').text(service.version);
+    $('#service-status').text(service.status);
+
+  },
+  'click #update-service': function (event) {
+
+    event.preventDefault();
+
+    // Get reference to template instance
+    const instance = Template.instance();
+
+    // Get service id reference
+    const serviceId = instance.serviceId;
+
+    // Update service data
+    Services.update(serviceId, {
+      $set: {
+        name: $('#service-name').text(),
+        description: $('#service-description').text(),
+        version: $('#service-version').text(),
+        status: $('#service-status').text()
+      }
+    });
+
+    // Deconstruct medium-editor
+    instance.serviceEditor.destroy();
+
+    // Update reactive variable
+    instance.editMode.set(false);
+  }
+})
